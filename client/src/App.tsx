ac2d2bc273e0648430cff11ceb31dca1f4835d88
Steiden <baldindaniilza@mail.ru>
2024-03-31 16:36:25 +0500

    Update testing client app for files sending Remove basic react styles, add new styles Add field of inputting files's uri

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,64 @@
 import { FormEvent, useState } from "react";
-import "./App.css";
 import axios, { AxiosResponse } from "axios";
+import "./App.css";
 
 function App() {
     const [files, setFiles] = useState<FileList | null>({} as FileList);
+    const [uri, setUri] = useState<string>("");
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
 
-        const fetchStart: number = Date.now();
+        const response: Promise<AxiosResponse> = axios.post(
+            "http://127.0.0.1:8000/api/files",
+            { 
+                files: files,
+                current_dir: uri },
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
 
-        const response: Promise<AxiosResponse> = axios.post("http://127.0.0.1:8000/api/files", { files }, 
-            { headers: { "Content-Type": "multipart/form-data", "Authorization": `Bearer ${localStorage.getItem("token")}` } });
-        
-        console.log((await response).data);
+        if ((await response).status !== 200) throw new Error((await response).statusText);
 
-        console.log(`Took ${Date.now() - fetchStart}ms to fetch files`);
+        const data = (await response).data;
+        console.log(data);
     };
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="files">Files</label>
-                <input type="file" name="files" id="files" multiple onChange={(e) => setFiles(e.target.files)} />
-                <button type="submit">Submit</button>
+            <form onSubmit={handleSubmit} className="file-form">
+                <div className="file-form__input-container">
+                    <label htmlFor="files" className="file-form__label">
+                        Files to send:
+                    </label>
+                    <input
+                        type="file"
+                        name="files"
+                        id="files"
+                        className=""
+                        multiple
+                        onChange={(e) => setFiles(e.target.files)}
+                    />
+                </div>
+                <div className="file-form__input-container">
+                    <label htmlFor="files" className="file-form__label">
+                        Uri (not required):
+                    </label>
+                    <input
+                        type="text"
+                        name="uri"
+                        id="uri"
+                        className="file-form__input"
+                        value={uri}
+                        onChange={(e) => setUri(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="file-form__button">
+                    Send files
+                </button>
             </form>
         </>
     );
